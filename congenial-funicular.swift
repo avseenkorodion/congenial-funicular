a44b79ce2cf3a45c2d8b08640bf6e76f5e8b5878
Cari Peck <152903975+avseenkorodion@users.noreply.github.com>
2023-12-27 21:09:23 +0100

    Create congenial-funicular.swift

---
+++ congenial-funicular.swift
@@ -0,0 +1,6 @@
+func path(in rect: CGRect) -> Path {
+        Path { path in
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - cornerRadius))
+            path.addArc(center: CGPoint(x: rect.maxX - cornerRadius, y: rect.maxY - cornerRadius),
